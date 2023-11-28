dfafc6efcbfafba76a745a5d1d6a7de58e82cfef
FujihitoWatanabe <fuzihito26@hotmail.com>
2023-11-28 18:05:55 +0900

    ポケモン図鑑
    Reactの学習でポケモン図鑑を作成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getAllPokemon, getPokemon } from './utils/pokemon.js';
+import { Card } from './components/Card/Card.js';
+import Navbar from './components/Navbar/Navbar.js';
 
 function App() {
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon/';
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [prevURL, setPrevURL] = useState('');
+  const [nextURL, setNextURL] = useState('');
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+      //各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      //データを取得時はローディング表示はなし
+      setLoading(false);
+      //console.log(res.next);
+      //前へのデータを取得
+      setPrevURL(res.previous); //Null
+      //次へのデータを取得
+      setNextURL(res.next);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    //ポケモンデータの詳細を取得
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+  //console.log(pokemonData);
+
+  //前へ遷移
+  const handlePrevPage = async () => {
+    if (!prevURL) return; //1ページ目はリターンとする
+    setLoading(true);
+    let data = await getAllPokemon(prevURL);
+    await loadPokemon(data.results);
+    setPrevURL(data.previous);
+    //データを取得時はローディング表示はなし
+    setLoading(false);
+  };
+
+  //次へ遷移
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous); // 2ページ以降に前へ移動
+    //データを取得時はローディング表示はなし
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>ロード中・・・</h1>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={handlePrevPage}>前へ</button>
+              <button onClick={handleNextPage}>次へ</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
